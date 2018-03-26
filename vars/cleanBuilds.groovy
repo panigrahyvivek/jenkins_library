f6a9efc9baf551623dbb8531bac4f1ba3faa6ae4
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-26 03:05:57 +0000

    1st

--- vars/cleanBuilds.groovy
+++ vars/cleanBuilds.groovy
@@ -1,8 +1,5 @@
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    echo 'Hello world'
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
