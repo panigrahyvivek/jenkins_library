3c7e9bca2a1b930c15880cf4418e4d8e4466042a
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 07:43:08 +0000

    loading shared library

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -1,13 +1,13 @@
 #!groovy
 
+@Library(['piper-library-os']) _
+
 def call(Map pipelineParams){
-	echo 'Hello'
 	node(){
 		dir(pipelineParams.src){
 			sh "/home/jenkinsuser/xsclient/bin/xs api ${pipelineParams.apiendpoint} --skip-ssl-validation"
 			sh "/home/jenkinsuser/xsclient/bin/xs login -u ${pipelineParams.username} -p ${pipelineParams.password} -o ${pipelineParams.org} -s ${pipelineParams.space}"
 			sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${pipelineParams.mtaFilePath}"
 		}
-		
 	}
 }
