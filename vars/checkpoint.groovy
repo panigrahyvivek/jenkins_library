7841943977ea5ddb7e02537deebc1bb59860ba40
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 09:05:10 +0000

    string comparision

--- vars/checkpoint.groovy
+++ vars/checkpoint.groovy
@@ -3,14 +3,14 @@
 def call(def system){
 	node(){
 		echo '12'
-		if(system == 'QA'){
+		if(system == "QA"){
 			echo '123'
 			timeout(time: 90, unit: 'SECONDS'){
 				input "Are you sure you want to promote the changes to ${system}"
 			}
 		}
 		
-		if(system == 'PROD'){
+		if(system == "PROD"){
 			timeout(time: 90, unit: 'SECONDS'){
 				input "Are you sure you want to promote the changes to ${system}"
 			}
