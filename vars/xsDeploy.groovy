cd37353b46caa5f26ba394de5e7017a6f921ea94
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 07:55:23 +0000

    update

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -1,13 +1,16 @@
 #!groovy
 
-@Library(['piper-library-os']) _
 
 def call(Map pipelineParams){
 	node(){
 		dir(pipelineParams.src){
-			sh "/home/jenkinsuser/xsclient/bin/xs api ${pipelineParams.apiendpoint} --skip-ssl-validation"
-			sh "/home/jenkinsuser/xsclient/bin/xs login -u ${pipelineParams.username} -p ${pipelineParams.password} -o ${pipelineParams.org} -s ${pipelineParams.space}"
-			sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${pipelineParams.mtaFilePath}"
+			
+			withCredentials([usernamePassword(credentialsId: 'xsadev', passwordVariable: 'password', usernameVariable: 'username')]) {
+			    sh "/home/jenkinsuser/xsclient/bin/xs api ${pipelineParams.apiendpoint} --skip-ssl-validation"
+				sh "/home/jenkinsuser/xsclient/bin/xs login -u ${username} -p ${password} -o ${pipelineParams.org} -s ${pipelineParams.space}"
+				sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${pipelineParams.mtaFilePath}"
+			}
+			
 		}
 	}
 }
