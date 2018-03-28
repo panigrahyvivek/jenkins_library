6ce3d3ca5234bb9f123435d7115389a4e9de4703
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 06:44:33 +0000

    xsDeploy update

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -6,7 +6,12 @@ import com.sap.piper.ConfigurationMerger
 
 @Library('piper-library-os') _
 
-node(){
-    
-    echo 'Hello World'
+
+def call(){
+	node(){
+	    mtaName='generic_xsa'
+		sh "/home/jenkinsuser/xsclient/bin/xs api ${XSA_API_ENDPOINT} --skip-ssl-validation"
+		sh "/home/jenkinsuser/xsclient/bin/xs login -u ${HANA_XSA_USER} -p ${HANA_XSA_PASSWORD} -o ${ORGANIZATION} -s ${CI_SPACE}"
+		sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${mtaName}.mtar"
+	}	
 }
