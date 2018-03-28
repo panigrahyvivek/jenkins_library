57b47d5e45afcee80bee5c2c481000dabc29db89
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 08:18:09 +0000

    updating credential config

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -4,7 +4,7 @@ def call(Map pipelineParams){
 	node(){
 		dir(pipelineParams.src){
 			
-			withCredentials([usernamePassword(credentialsId: pipelineParams.xsHome, passwordVariable: 'password', usernameVariable: 'username')]) {
+			withCredentials([usernamePassword(credentialsId: pipelineParams.credentialsId, passwordVariable: 'password', usernameVariable: 'username')]) {
 			    sh "${pipelineParams.xsHome}/xs api ${pipelineParams.apiendpoint} --skip-ssl-validation"
 				sh "${pipelineParams.xsHome}/xs login -u ${username} -p ${password} -o ${pipelineParams.org} -s ${pipelineParams.space}"
 				sh "${pipelineParams.xsHome}/xs deploy -f ${pipelineParams.mtaFilePath}"
