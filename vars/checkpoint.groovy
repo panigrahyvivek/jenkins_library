817f386a082908111a2020d4bb9d5d0ebcdd1e48
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 09:01:57 +0000

    dd

--- vars/checkpoint.groovy
+++ vars/checkpoint.groovy
@@ -2,8 +2,9 @@
 
 def call(def system){
 	node(){
-		
-		if(system == 'QA'){
+		echo '12'
+		if(system = 'QA'){
+			echo '123'
 			timeout(time: 90, unit: 'SECONDS'){
 				input "Are you sure you want to promote the changes to ${system}"
 			}
