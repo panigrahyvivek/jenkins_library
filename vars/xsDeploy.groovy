39c7edfc6af89b5da371b91ca21e75fda68705d9
PANIGRAHYV <vivekananda.panigrahy@tyson.com>
2018-03-28 07:19:21 +0000

    update

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -3,9 +3,8 @@
 def call(Map pipelineParams){
 	echo 'Hello'
 	node(){
-	    mtaName='generic_xsa'
-		sh "/home/jenkinsuser/xsclient/bin/xs api ${pipelineParams.XSA_API_ENDPOINT} --skip-ssl-validation"
-		sh "/home/jenkinsuser/xsclient/bin/xs login -u ${pipelineParams.HANA_XSA_USER} -p ${pipelineParams.HANA_XSA_PASSWORD} -o ${pipelineParams.ORGANIZATION} -s ${pipelineParams.CI_SPACE}"
-		sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${mtaName}.mtar"
+		sh "/home/jenkinsuser/xsclient/bin/xs api ${pipelineParams.apiendpoint} --skip-ssl-validation"
+		sh "/home/jenkinsuser/xsclient/bin/xs login -u ${pipelineParams.username} -p ${pipelineParams.password} -o ${pipelineParams.org} -s ${pipelineParams.space}"
+		sh "/home/jenkinsuser/xsclient/bin/xs deploy -f ${pipelineParams.mtaName}.mtar"
 	}
 }
